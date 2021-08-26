6f88038c4bde13e7a5355e340c9930519adc82ae
Carlos Cuilty <a01721636@itesm.mx>
2021-08-26 15:27:09 -0500

    Programas Completos

--- assignments/03TipoTriangulo/src/exercise.py
+++ assignments/03TipoTriangulo/src/exercise.py
@@ -4,6 +4,19 @@ def main():
     lado3 = int(input("Ingresa la medida del lado 3: "))
     #Escribe aquí tu código...
 
+    if lado1 > 0 and lado2 > 0 and lado3 > 0:
+        if (lado1 + lado2 > lado3) and (lado1 + lado3 > lado2) and (lado3 + lado2 > lado1):
+            if lado1 == lado2 and lado1 == lado3 and lado2 == lado3: #Para ver si es equilatero
+                print('ES UN TRIANGULO EQUILATERO')
+            elif lado1 != lado2 and lado1 != lado3 and lado2 != lado3:
+                print('ES UN TRIANGULO ESCALENO')
+            else:
+                print('ES UN TRIANGULO ISOSCELES')
+        else:
+            print('NO ES TRIANGULO')
+    else:
+        print('NO ES TRIANGULO')
+
 
 if __name__=='__main__':
     main()
